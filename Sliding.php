0a6fe4b1bfce08a53b9be7d9bc027cddad6d806c
Lorenzo Alberton <quipo@php.net>
2006-01-20 13:43:00 +0000

    bump (C) years
    git-svn-id: https://svn.php.net/repository/pear/packages/Pager/trunk@205625 c90b9560-bf6c-de11-be94-00142212c4b1

--- Sliding.php
+++ Sliding.php
@@ -30,7 +30,7 @@
  * @category   HTML
  * @package    Pager
  * @author     Lorenzo Alberton <l dot alberton at quipo dot it>
- * @copyright  2003-2005 Lorenzo Alberton
+ * @copyright  2003-2006 Lorenzo Alberton
  * @license    http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
  * @version    CVS: $Id$
  * @link       http://pear.php.net/package/Pager

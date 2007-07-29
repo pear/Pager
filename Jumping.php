d3b88c5438221f7ee2fc7b87fa6021236e113f63
Lorenzo Alberton <quipo@php.net>
2007-07-29 08:13:02 +0000

    fix last commit
    git-svn-id: https://svn.php.net/repository/pear/packages/Pager/trunk@240437 c90b9560-bf6c-de11-be94-00142212c4b1

--- Jumping.php
+++ Jumping.php
@@ -75,41 +75,6 @@ class Pager_Jumping extends Pager_Common
     }
 
     // }}}
-    // {{{ build()
-
-    /**
-     * Generate or refresh the links and paged data after a call to setOptions()
-     *
-     * @access public
-     */
-    function build()
-    {
-        //reset
-        $this->_pageData = array();
-        $this->links = '';
-
-        $this->_generatePageData();
-        $this->_setFirstLastText();
-
-        if ($this->_totalPages > (2 * $this->_delta + 1)) {
-            $this->links .= $this->_printFirstPage();
-        }
-
-        $this->links .= $this->_getBackLink();
-        $this->links .= $this->_getPageLinks();
-        $this->links .= $this->_getNextLink();
-
-        if ($this->_totalPages > (2 * $this->_delta + 1)) {
-            $this->links .= $this->_printLastPage();
-        }
-
-        $this->linkTags .= $this->_getFirstLinkTag();
-        $this->linkTags .= $this->_getPrevLinkTag();
-        $this->linkTags .= $this->_getNextLinkTag();
-        $this->linkTags .= $this->_getLastLinkTag();
-    }
-
-    // }}}
     // {{{ getPageIdByOffset()
 
     /**
@@ -196,7 +161,6 @@ class Pager_Jumping extends Pager_Common
         }
 
         if (!is_null($pageID)) {
-
             $this->links = '';
             if ($this->_totalPages > $this->_delta) {
                 $this->links .= $this->_printFirstPage();

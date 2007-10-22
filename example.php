14163f54e3e242a32ed1b92440f4ef3840431b4a
Lorenzo Alberton <quipo@php.net>
2007-10-22 14:07:01 +0000

    avoid short opening tags
    git-svn-id: https://svn.php.net/repository/pear/packages/Pager/trunk@244610 c90b9560-bf6c-de11-be94-00142212c4b1

--- example.php
+++ example.php
@@ -1,36 +1,19 @@
 <?php
-// +-----------------------------------------------------------------------+
-// | Copyright (c) 2002-2003, Richard Heyes                                     |
-// | All rights reserved.                                                  |
-// |                                                                       |
-// | Redistribution and use in source and binary forms, with or without    |
-// | modification, are permitted provided that the following conditions    |
-// | are met:                                                              |
-// |                                                                       |
-// | o Redistributions of source code must retain the above copyright      |
-// |   notice, this list of conditions and the following disclaimer.       |
-// | o Redistributions in binary form must reproduce the above copyright   |
-// |   notice, this list of conditions and the following disclaimer in the |
-// |   documentation and/or other materials provided with the distribution.|
-// | o The names of the authors may not be used to endorse or promote      |
-// |   products derived from this software without specific prior written  |
-// |   permission.                                                         |
-// |                                                                       |
-// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   |
-// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     |
-// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR |
-// | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  |
-// | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, |
-// | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT      |
-// | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, |
-// | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY |
-// | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT   |
-// | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE |
-// | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  |
-// |                                                                       |
-// +-----------------------------------------------------------------------+
-// | Author: Richard Heyes <richard@phpguru.org>                           |
-// +-----------------------------------------------------------------------+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Sample usage of the Pager class
+ *
+ * PHP versions 4 and 5
+ *
+ * @category   HTML
+ * @package    Pager
+ * @author     Richard Heyes <richard@phpguru.org>
+ * @copyright  2003 Richard Heyes
+ * @license    http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/Pager
+ */
 
 include 'Pager.php';
 
@@ -50,15 +33,15 @@ list($from, $to) = $pager->getOffsetByPageId();
 <table border="0" width="100%">
 	<tr>
 		<td colspan="3" align="center">
-			Displaying [<?=$from?> - <?=$to?>] of <?=$pager->_totalItems?> <br />
-			<?=$links['pages']?>
+			Displaying [<?php echo $from; ?> - <?php echo $to; ?>] of <?php echo $pager->_totalItems; ?> <br />
+			<?php echo $links['pages']; ?>
 		</td>
 	</tr>
 
 	<tr>
-		<td><nobr><?=$links['back']?></nobr></td>
+		<td><nobr><?php echo $links['back']; ?></nobr></td>
 		<td align="center" width="100%">&nbsp;</td>
-		<td align="right"><nobr><?=$links['next']?></nobr></td>
+		<td align="right"><nobr><?php echo $links['next']; ?></nobr></td>
 	</tr>
 
 	<tr>
@@ -70,14 +53,14 @@ list($from, $to) = $pager->getOffsetByPageId();
 
 <h4>Results from methods:</h4>
 <pre>
-getCurrentPageID()...: <?=$pager->getCurrentPageID()?> 
-getNextPageID()......: <?var_dump($pager->getNextPageID())?>
-getPreviousPageID()..: <?var_dump($pager->getPreviousPageID())?>
-numItems()...........: <?var_dump($pager->numItems())?>
-numPages()...........: <?var_dump($pager->numPages())?>
-isFirstPage()........: <?var_dump($pager->isFirstPage())?>
-isLastPage().........: <?var_dump($pager->isLastPage())?>
-isLastPageComplete().: <?var_dump($pager->isLastPageComplete())?>
+getCurrentPageID()...: <?php echo $pager->getCurrentPageID(); ?>
+getNextPageID()......: <?php var_dump($pager->getNextPageID()); ?>
+getPreviousPageID()..: <?php var_dump($pager->getPreviousPageID()); ?>
+numItems()...........: <?php var_dump($pager->numItems()); ?>
+numPages()...........: <?php var_dump($pager->numPages()); ?>
+isFirstPage()........: <?php var_dump($pager->isFirstPage()); ?>
+isLastPage().........: <?php var_dump($pager->isLastPage()); ?>
+isLastPageComplete().: <?php var_dump($pager->isLastPageComplete()); ?>
 </pre>
 
 </body>

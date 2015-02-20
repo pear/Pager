3bff4dc50102e9fb570e3f5665d4881c8ee34a3d
Christian Weiske <cweiske@cweiske.de>
2015-02-20 13:25:58 +0100

    ignore deprecated errors

--- tests/all_tests.php
+++ tests/all_tests.php
@@ -4,7 +4,7 @@
 require_once 'simple_include.php';
 require_once 'pager_include.php';
 
-error_reporting(error_reporting() & ~E_STRICT);
+error_reporting(error_reporting() & ~E_STRICT & ~E_DEPRECATED);
 
 class AllTests extends TestSuite {
     function AllTests() {
